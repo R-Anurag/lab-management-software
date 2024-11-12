8b1a3101586cb427d4d449beb6cba5fa476d38e4
Anurag Rai <146112209+R-Anurag@users.noreply.github.com>
2024-11-12 18:56:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:anuragrai123@localhost/test_database'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://tgfvmycch3ud6npk:ms9givan0npjldga@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/udmcqa9h0lxu08ai'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://u81j7f0feiq2bu:pdcc96988611981c4bb8d2c5d554cc8e5baab9a3ea3c0e6901a9320d308796ac8@c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7pna5850fo08p'
 
 
 # Creating database object
